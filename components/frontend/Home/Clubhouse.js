cac6d76eda0980ff8763b58d343a46f0030b5a71
harshithva <harshith11032001@gmail.com>
2022-05-18 09:26:16 +0530

    done

--- components/frontend/Home/Clubhouse.js
+++ components/frontend/Home/Clubhouse.js
@@ -8,7 +8,7 @@ const Clubhouse = () => {
             <div className="row">
                     <div className="col-md-6 col-sm-6" data-scroll-reveal="enter left and move 20px" style={{margin:"auto"}}>
                     <h2 style={{fontFamily:'Montserrat',color:'white',fontSize:'42px'}}>ClubHouse</h2>
-                        <h2 style={{fontWeight:"600",fontSize:"1.3em",fontFamily:'Montserrat',lineHeight:'1.6em'}}>One of its kind high end clubhouse giving a beautiful view of the surroundings. Facilitated with 7 luxury Suite Rooms, Cafe & Family Restaurant with Centralized AC.</h2>
+                        <h2 style={{fontWeight:"600",color:"white",fontSize:"1.3em",fontFamily:'Montserrat',lineHeight:'1.6em'}}>One of its kind high end clubhouse giving a beautiful view of the surroundings. Facilitated with 7 luxury Suite Rooms, Cafe & Family Restaurant with Centralized AC.</h2>
                         
                         {/* <Button color={"red"} onClick={downloadFile}>
                           <i style={{marginRight:"0.5em"}} className="fa fa-download"></i> Download Brochure
