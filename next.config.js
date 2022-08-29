4f2a0dc0f2ee1e00035f2f6e3cda2d83ccc3d8c3
harshithva <harshith11032001@gmail.com>
2022-08-29 12:20:14 +0530

    done

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['yamuna-bucket.s3.ap-south-1.amazonaws.com','firebasestorage.googleapis.com','yamunaashacity.com','127.0.0.1','admin.yamunaashacity.com'],
+    domains: ['vajra.bycomsolutions.com','yamuna-bucket.s3.ap-south-1.amazonaws.com','firebasestorage.googleapis.com','yamunaashacity.com','127.0.0.1','admin.yamunaashacity.com'],
   },
   eslint: {
     // Warning: This allows production builds to successfully complete even if
--- utilities/api.js
+++ utilities/api.js
@@ -1,4 +1,4 @@
-const url = 'https://admin.yamunaashacity.com'
+const url = 'https://vajra.bycomsolutions.com'
 // const url = 'http://127.0.0.1:8000'
 
 export const getHomepageSettings = async()=>{
